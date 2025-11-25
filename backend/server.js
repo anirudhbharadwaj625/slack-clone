82507f89b21701953085d86d01b47bab37cef4e5
Your Name <anirudhbharadwaj0326@gmail.com>
2025-11-25 14:02:40 +0530

    auth implemented

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,32 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/injest.js";
+import { serve } from "inngest/express";
 const app = express();
-
+app.use(express.json());
+app.use(clerkMiddleware()); //req,auth() will be availebe in the request object
 // console.log(PORT);
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-app.listen(ENV.PORT, () => {
-  console.log("Server is running on port ", ENV.PORT);
-  connectDB();
-});
+
+const startServer = async () => {
+  try {
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server is running on port ", ENV.PORT);
+        connectDB();
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
